c15b96410ec1deda37a023467ca5dac712e9efc2
Bolleboina-Srikanth <srikanthbolleboina456@gmail.com>
2023-10-01 19:25:01 +0530

    [Srikanth] Add: Added form validation

--- src/COMPONENTS/PAGES/SignIn/SignIn.jsx
+++ src/COMPONENTS/PAGES/SignIn/SignIn.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import './XSignin.css';
 import CloseIcon from '@mui/icons-material/CloseOutlined';
 import ximage from '../../IMAGES/twitterImage.png';
@@ -7,6 +7,44 @@ import signinappleimg from '../../IMAGES/appleImage.png';
 import TextField from '@mui/material/TextField';
 import { Link } from "@mui/material";
 function XSignin({set}) {
+
+    const[loginINput, setLoginInput] = useState(
+        {
+            email : "",
+            password : '',
+        }
+    )
+    let name,value;
+
+    const handleinput = (e) =>{
+          name=e.target.name;
+          value=e.target.value;
+          setLoginInput({...loginINput,[name]:value})
+    }
+ // -----------------------------------------
+//  const mobileRegex = /^[6-9]\d{9}$/;
+const emailRegex = /^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*$/;
+ const passwordRegex = /^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$/;
+ let emailTest = emailRegex.test(loginINput. email);
+ let passwordTest = passwordRegex.test(loginINput.password);
+ const[errorObj, setErrorObj] =useState({
+
+ });
+  const handleprint = () =>{
+       setErrorObj((prevstate) =>(
+        {
+            ...prevstate,
+            emailError : !emailTest,
+            emailHelperText : !emailTest ? "enter correct phone number" : "",
+
+            passwordError: !passwordTest,
+            passwordHelperText: !passwordTest ? "enter correct password" : ""
+
+        }
+       ))
+
+  }
+//   ---------------------------------------------------------------------
     const handletohome = () =>{
        set(false);
     }
@@ -30,11 +68,14 @@ function XSignin({set}) {
                        <button id="xsigninbuttonsize"><img id="xsigninsize" src={signingoogleimg} />&nbsp;Sign in with Google</button><br></br>
                         <button id="xapplebutton"><img id="xsigninimgsize" src={signinappleimg} />&nbsp;Sign in with Apple</button>
                         <p>---------------------------or------------------------</p>
-                        <TextField id="xoutlined-basic" label="phone, email or username"/>
-                        <br></br>
-                        <button id="xnext">Next</button><br></br>
-                        <button id="xfpwd">Forgot password?</button><br/><br/>
-                        <div className="dont">Don't have an account?<a href="#">Sign up</a></div>
+                        <div className="XLOGINFIELDS">
+                        <TextField id="xoutlined-basic" label="email" name="email" value={loginINput.email} onChange={handleinput} error={errorObj.emailError} helperText={errorObj.emailHelperText}/>
+                        <TextField id="xoutlined-basic" label="password" name="password" value={loginINput.password} onChange={handleinput} error={errorObj.passwordError} helperText={errorObj.passwordHelperText}/>
+                        </div>
+                        <button id="xnext" onClick={handleprint}>Next</button><br></br>
+                        <button id="xfpwd">Forgot password?</button><br/>
+                        
+                        <div className="dont" >Don't have an account?<a href="#">Sign up</a></div>
                       </div>
                 </div>
 
--- src/COMPONENTS/PAGES/SignUp/Xsignup.jsx
+++ src/COMPONENTS/PAGES/SignUp/Xsignup.jsx
@@ -1,13 +1,75 @@
-import React from "react";
+import React, { useState } from "react";
 import './Xsignup.css';
 import CloseIcon from '@mui/icons-material/CloseOutlined';
 import TextField from '@mui/material/TextField';
 
-function Xsignup({valueReceived}) {
+function Xsignup({ valueReceived }) {
 
-   const handletohome = () =>{
-    valueReceived(false);
-   }
+    const nameRegex = /^[a-zA-Z ]{2,30}$/;
+    const mobileRegex = /^[6-9]\d{9}$/;
+    const emailRegex = /^[a-z]{3,}(.[0-9a-z]*)?@([a-z]){2,}.[a-z]+(.in)*$/;
+    const passwordRegex = /^.*(?=.{8,})(?=.*[A-Z])(?=.*[0-9])(?=.*[@#$%^&+=]).*$/;
+
+
+    const [userInput, setUserInput] = useState(
+        {
+            Name: "",
+            email: "",
+            password: "",
+            confirmPassword: ""
+        }
+    )
+
+    let name, value;
+
+    const handleInput = (e) => {
+        name = e.target.name;
+        value = e.target.value;
+        setUserInput({ ...userInput, [name]: value })
+    }
+
+
+    //    -------------------------------------------------
+    const [errorObj, setErrorObj] = useState(
+        {
+
+        }
+    )
+    const handlepage = () => {
+        let nameTest = nameRegex.test(userInput.Name);
+        let emailTest = emailRegex.test(userInput.email);
+        let passwordtest = passwordRegex.test(userInput.password);
+        let confirmPasswordtest = passwordRegex.test(userInput.confirmPassword);
+
+        setErrorObj((prevstate) => (
+            {
+                ...prevstate,
+                NameError: !nameTest,
+                Nameheplertext: !nameTest ? "enter correct name" : "",
+
+                emailError: !emailTest,
+                emailhelpertext: !emailTest ? "enter valid email" : "",
+
+                passwordError: !passwordtest,
+                passwordHelperText: !passwordtest ? "enter correct password" : "",
+
+                confirmPasswordError: !confirmPasswordtest,
+                confirmPasswordHelperText: !confirmPasswordtest ? "enter correct password" : ""
+
+
+            }
+        ))
+
+    }
+
+
+    // ----------------------------------------------------
+
+
+
+    const handletohome = () => {
+        valueReceived(false);
+    }
 
     return (
         <div className="xsignupfull-screen">
@@ -15,22 +77,28 @@ function Xsignup({valueReceived}) {
                 <div className="xAndStep">
 
                     <CloseIcon href="#" className="closeicon" onClick={handletohome}></CloseIcon>
-                    <div className="steptext">Step 1 of 5</div>
+                    <div className="steptext">Step 1 of 2</div>
                 </div>
                 <div className="xsignupchild2">
-                    <div id="xcrtacct">Create your account</div><br /><br />
-                    <TextField id="xTextoutlined-basic" label="Name" />
-                    <br />
-                    <TextField id="xTextoutlined-basic" label="Phone" /><br/>
-                    <div className="xuseEmail"><a href="#">Use email instead</a></div><br/>
+                    <div id="xcrtacct">Create your account</div>
+                    <div className="xinputfiels">
+                    <TextField id="xTextoutlined-basic" label="Name" onChange={handleInput} name="Name" value={userInput.Name} error={errorObj.NameError} helperText={errorObj.Nameheplertext} />
+                    <div>
+                    <TextField id="xTextoutlined-basic" label="email" onChange={handleInput} name="email" value={userInput.email} error={errorObj.emailError} helperText={errorObj.emailhelpertext} />
+                    
+                    {/* <div className="xuseEmail"><a href="#">Use email instead</a></div> */}
+                    </div>
+                    <TextField id="xTextoutlined-basic" label="Password" onChange={handleInput} name="password" value={userInput.password} error={errorObj.passwordError} helperText={errorObj.passwordHelperText} />
+                    <TextField id="xTextoutlined-basic" label="Confirm password" onChange={handleInput} name="confirmPassword" value={userInput.confirmPassword} error={errorObj.confirmPasswordError} helperText={errorObj.confirmPasswordHelperText} />
+                    </div>
                     <div id="xsignupdate">Date of birth</div>
                     <div className="xcrtacctfulltext">
-                    This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.
-                    </div><br/>
+                        This will not be shown publicly. Confirm your own age, even if this account is for a business, a pet, or something else.
+                    </div><br />
                     <div className="">
-                    <input id="xdate" type="date"></input>
-                    </div><br/><br/><br/>
-                    <button className="xsignupnext">Next</button>
+                        <input id="xdate" type="date"></input>
+                    </div><br />
+                    <button className="xsignupnext" onClick={handlepage}>Next</button>
                 </div>
 
 
